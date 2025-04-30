edb42885c852fa0b27b008d56cf399615e3e8aa6
Suhun0331 <rlatngns6046@naver.com>
2025-04-30 16:35:23 +0900

    First Setting

---
+++ src/main/java/com/quickpick/ureca/UrecaApplication.java
@@ -0,0 +1,13 @@
+package com.quickpick.ureca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quickpick/ureca/UrecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickpick.ureca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
