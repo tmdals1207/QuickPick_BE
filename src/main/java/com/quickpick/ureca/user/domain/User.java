1a2c7b34a3e3788dfc761c8957eed10f6e33d285
etoile0626 <etoile0626@naver.com>
2025-04-30 22:12:49 +0900

    user(연관관계 미완성), reserve 객체 추가

---
+++ src/main/java/com/quickpick/ureca/user/domain/User.java
@@ -0,0 +1,50 @@
+package com.quickpick.ureca.user.domain;
+
+import com.quickpick.ureca.reserve.domain.Reserve;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Table(name = "user")
+@Entity
+@Getter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(nullable = false)
+    private String id;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String age;
+
+    @Column(nullable = false)
+    private String gender;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime created_at;
+
+    @LastModifiedDate
+    private LocalDateTime updated_at;
+
+    @OneToMany
+    @JoinColumn(name = "reserve_id")
+    private List<Reserve> reserveList = new ArrayList<>();
+}
