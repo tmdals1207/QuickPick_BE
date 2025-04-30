7946e25ec948cb9398e12d313e707e234862e389
etoile0626 <etoile0626@naver.com>
2025-04-30 22:12:49 +0900

    user(연관관계 미완성), reserve 객체 추가 (#1)

---
+++ src/main/java/com/quickpick/ureca/reserve/domain/Reserve.java
@@ -0,0 +1,35 @@
+package com.quickpick.ureca.reserve.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Table(name = "user")
+@Entity
+@Getter
+@NoArgsConstructor
+public class Reserve {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "reserve_id")
+    private Long reserveId;
+
+    @Column(name = "user_id", nullable = false)
+    private String userId;
+
+    @Column(nullable = false)
+    private String status;
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime created_at;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updated_at;
+}
---
+++ src/main/java/com/quickpick/ureca/user/service/UserService.java
@@ -0,0 +1,4 @@
+package com.quickpick.ureca.user.service;
+
+public class UserService {
+}
