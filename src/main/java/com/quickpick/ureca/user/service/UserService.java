c20107d3737e3518753ae5c64bcd11e223bfa100
etoile0626 <etoile0626@naver.com>
2025-05-01 15:37:09 +0900

    feat: @Transactional 추가

--- src/main/java/com/quickpick/ureca/user/service/UserService.java
+++ src/main/java/com/quickpick/ureca/user/service/UserService.java
@@ -3,6 +3,7 @@ package com.quickpick.ureca.user.service;
 import com.quickpick.ureca.user.domain.User;
 import com.quickpick.ureca.user.dto.UserSignUpRequestDto;
 import com.quickpick.ureca.user.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,7 @@ public class UserService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    @Transactional
     public void saveUser(UserSignUpRequestDto dto) {
         userRepository.save(User.builder()
                 .id(dto.getId())
